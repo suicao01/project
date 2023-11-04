7b2e9e451ca8a4dbfb047a14383bd08205b164ce
suicao01 <anhthoho000@gmail.com>
2023-11-04 19:06:36 +0700

    add file

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -2,18 +2,31 @@ package com.example.demo;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
-public class Controller {
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
 
+
+public class Controller implements Initializable{
+
+
+
+    @FXML
+    private ListView<String> listWord;
     @FXML
     TextField search;
     @FXML
@@ -24,23 +37,54 @@ public class Controller {
     Stage stage;
     @FXML
     Label meaning;
-
+    @FXML
+    Label function;
+    @FXML
+    private Button history;
     @FXML
     Text textMeaning;
+    private final String path = "history.txt";
+
+    protected DictionaryManagement getManage() throws IOException {
+        DictionaryManagement d = new DictionaryManagement();
+        d.insertFile();
+        return d;
+    }
+
     @FXML
     public void Logout(ActionEvent event) {
-       stage = (Stage) ScenePane.getScene().getWindow();
-       stage.close();
+        stage = (Stage) ScenePane.getScene().getWindow();
+        stage.close();
     }
 
     // tim ra tu tieng anh
-    public void findWord(ActionEvent event) throws IOException {
-        DictionaryManagement d = new DictionaryManagement();
-        d.insertFile();
+    public void findWord() throws IOException {
         String word = search.getText();
-        //d.changeWord("love","Yen Ngoc");
-        meaning.setText(d.getWordLookedUp(word));
-        textMeaning.setText(d.dictionaryLookup(word));
+        if (word.isEmpty()) {
+            textMeaning.setText("");
+            meaning.setText("");
+        }
+
+        meaning.setText(getManage().getWordLookedUp(word));
+        textMeaning.setText(getManage().dictionaryLookup(word));
+        if (!word.isEmpty()) {
+            getManage().exportToFile(word, path);
+        }
     }
 
+    // hien ra man hinh lich su tim kiem
+    public void hisScreen(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("his.fxml")));
+
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
